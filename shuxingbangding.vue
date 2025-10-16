a0a299645808feae04f9518f807d5633e6170d3c
suching8848 <1579290250@qq.com>
2025-10-16 16:43:09 +0800

    10/16

---
+++ shuxingbangding.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="shuxingbangding">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>属性绑定</h1>
+            </div>
+        </div>
+    </div>
